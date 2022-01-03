ead2dd24eb1e309f057cc025c6715094f90b6bf7
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-03 16:07:41 +0100

    first commit

---
+++ EmojiArtWork/EmojiArtWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtWorkApp.swift
+//  EmojiArtWork
+//
+//  Created by Amjad Oudeh on 03.01.22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
