1a4c3a55687ce67ba8b64fd95971ae44c6985d8f
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-03 17:08:18 +0100

    Created a function to add Emoji to the Model and to make the id accessible I created an init has the exact same vars and assign the values exactly the same as well. so and I made it file-private

--- EmojiArtWork/EmojiArtWorkModel.swift
+++ EmojiArtWork/EmojiArtWorkModel.swift
@@ -10,17 +10,37 @@ import Foundation
 struct EmojiArtWorkModel {
     
     // our model have 2 parts the background and the Emojis
-    var background: Background
+    var background =  Background.blank
     var emojis = [Emoji]()
     
-    struct Emoji {
+    struct Emoji: Identifiable {
         // text for the emoji shape
         let text: String
         // the position and the size
         var x: Int
         var y: Int
         var size: Int
+        let id: Int
+        
+        
+        // to make the id accessable we have to create an init has the exact same vars and asigin the valuse exactly the same as well. so we make fileprivate
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int){
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
     }
+    
+    init() { }
+    // a function to add Emoij to the Model
+    private var uniqueEmojiId = 0
+    mutating func addEmoji(_ text: String, at location: (x:Int , y:Int), size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x:  location.x, y: location.y, size: size, id: uniqueEmojiId))
+    }
+    
     // the background could be blank or link (url) or ImageData(typical image)
     
     enum Background {
@@ -29,3 +49,4 @@ struct EmojiArtWorkModel {
         case imageData
     }
 }
+
