7b604ae9f3dda4d345ceaffeda62a40034b4aac2
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-01-05 09:18:32 +0100

    Renaming the File form " ContentView " to " EmojiArtWorkDocumentView". here is or View file :)

--- EmojiArtWork/EmojiArtWorkApp.swift
+++ EmojiArtWork/EmojiArtWorkApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EmojiArtWorkApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiArtWorkDocumentView()
         }
     }
 }
